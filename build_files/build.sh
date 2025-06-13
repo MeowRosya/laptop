ca8a7a5a03c2109f864488c1dcf0ec246cf0b164
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2025-06-13 11:11:03 +0000

    base

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 remove -y firefox
+
+dnf5 install -y google-noto-fonts-all gnome-tweaks papirus-icon-theme adw-gtk3-theme
 
 # Use a COPR Example:
 #
