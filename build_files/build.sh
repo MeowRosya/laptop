04ced5660a0ee094d890cc8fa5a0405c9a812e93
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2025-08-14 10:53:37 +0000

    add ghostty

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,6 +20,10 @@ dnf5 group install -y --with-optional virtualization
 
 dnf5 install -y niri xwayland-satellite waybar rofi-wayland network-manager-applet blueman
 
+dnf5 -y copr enable scottames/ghostty
+dnf5 -y install ghostty
+dnf5 -y copr disable scottames/ghostty
+
 # dnf5 install -y askpass
 
 # dnf5 install -y gamescope
