c55766d26225553d7b3c15e1a63fbe52bd7e8d86
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2025-08-21 15:13:59 +0000

    move to bazzite

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,32 +10,29 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 remove -y firefox
 
-dnf5 install -y google-noto-fonts-all gnome-tweaks
+dnf5 install -y google-noto-fonts-all gtk-murrine-engine sassc
 
-dnf5 install -y openssl
+# dnf5 group install -y --with-optional virtualization
 
-dnf5 group install -y --with-optional virtualization
+# dnf5 install -y niri xwayland-satellite waybar rofi-wayland network-manager-applet blueman
 
-dnf5 install -y niri xwayland-satellite waybar rofi-wayland network-manager-applet blueman
+# dnf5 install -y sassc swaybg pavucontrol
 
-dnf5 install -y sassc swaybg pavucontrol
+# dnf5 -y copr enable scottames/ghostty
+# dnf5 -y install ghostty
+# dnf5 -y copr disable scottames/ghostty
 
-dnf5 -y copr enable scottames/ghostty
-dnf5 -y install ghostty
-dnf5 -y copr disable scottames/ghostty
+# dnf5 -y copr enable erikreider/SwayNotificationCenter
+# # Or latest stable release or -git package
+# dnf5 -y install SwayNotificationCenter
+# dnf5 -y copr disable erikreider/SwayNotificationCenter
 
-dnf5 -y copr enable erikreider/SwayNotificationCenter
-# Or latest stable release or -git package
-dnf5 -y install SwayNotificationCenter
-dnf5 -y copr disable erikreider/SwayNotificationCenter
+# dnf5 install -y ripgrep fd-find fzf neovim
 
-dnf5 install -y ripgrep fd-find fzf neovim
-
-dnf5 -y copr enable dejan/lazygit
-dnf5 -y install lazygit
-dnf5 -y copr disable dejan/lazygit
+# dnf5 -y copr enable dejan/lazygit
+# dnf5 -y install lazygit
+# dnf5 -y copr disable dejan/lazygit
 
 # dnf5 install -y askpass
 
