ed6e9ebcc657f10392ec9ecb156211533d53a7be
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2025-08-23 21:26:14 +0000

    remove systemc firefox

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,6 +11,8 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 
+dnf5 remove -y firefox
+
 dnf5 install -y google-noto-fonts-all gtk-murrine-engine sassc
 
 # dnf5 group install -y --with-optional virtualization
