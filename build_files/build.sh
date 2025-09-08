8d623bba802c746413f055919a7415d329be6631
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2025-09-08 07:02:07 +0000

    add deps for custom gtk themes

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,45 +14,7 @@ set -ouex pipefail
 # use instead firefox via flatpak
 dnf5 remove -y firefox
 
-dnf5 install -y openssl
-
-# dnf5 install -y google-noto-fonts-all
-
-# dnf5 group install -y --with-optional virtualization
-
-# dnf5 install -y niri xwayland-satellite waybar rofi-wayland network-manager-applet blueman
-
-# dnf5 install -y sassc swaybg pavucontrol
-
-# dnf5 -y copr enable scottames/ghostty
-# dnf5 -y install ghostty
-# dnf5 -y copr disable scottames/ghostty
-
-# dnf5 -y copr enable erikreider/SwayNotificationCenter
-# # Or latest stable release or -git package
-# dnf5 -y install SwayNotificationCenter
-# dnf5 -y copr disable erikreider/SwayNotificationCenter
-
-# dnf5 install -y ripgrep fd-find fzf neovim
-
-# dnf5 -y copr enable dejan/lazygit
-# dnf5 -y install lazygit
-# dnf5 -y copr disable dejan/lazygit
-
-# dnf5 install -y askpass
-
-# dnf5 install -y gamescope
-
-# NIRI
-# dnf5 install -y niri xwayland-satellite
-
-# dnf5 -y copr enable solopasha/hyprland
-# dnf5 -y install swww
-# dnf5 -y copr disable solopasha/hyprland
-
-# dnf5 -y copr enable errornointernet/quickshell
-# dnf5 -y install quickshell
-# dnf5 -y copr disable errornointernet/quickshell
+dnf5 install -y openssl gtk-murrine-engine sassc
 
 # Use a COPR Example:
 #
