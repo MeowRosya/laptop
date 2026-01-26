04bffa808c9a11e8a0a5d32c5b7ef060c5c87b68
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2026-01-26 16:38:14 +0000

    move to kinoite

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,12 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-
-# use instead firefox flatpak
-dnf5 remove -y firefox
-
-dnf5 install -y dnscrypt-proxy
+# this installs a package from fedora repos 
 
 # Use a COPR Example:
 #
