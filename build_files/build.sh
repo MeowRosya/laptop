6b515c32e2ce143afaee0258e1ec62e5901016e3
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2026-01-26 17:17:00 +0000

    add virt-manager

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,6 +11,8 @@ set -ouex pipefail
 
 # this installs a package from fedora repos 
 
+dnf5 group install --with-optional virtualization
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
