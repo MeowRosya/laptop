d93d3cacd7d6c41238614730f708581f72751368
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2026-02-15 11:24:39 +0300

    Comment out dnf5 check-update command
    Comment out the dnf5 check-update command in build.sh.

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-dnf5 -y check-update
+# dnf5 -y check-update
 
 # this installs a package from fedora repos 
 
