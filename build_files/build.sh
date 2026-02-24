05928deafb8efe97e0819d03744bd716b6f8ff9e
Elizarov Yaroslav <132341650+MeowRosya@users.noreply.github.com>
2026-02-25 01:55:25 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -37,7 +37,7 @@ dnf5 -y install steam
 
 dnf5 -y install curl cabextract xorg-x11-font-utils fontconfig
 
-rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
+rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
 fc-cache -fv
 
